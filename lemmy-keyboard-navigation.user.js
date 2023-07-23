e5d5e572eeaa9c5b160e2d55ec7e866ad996fb50
InfinibyteF4 <75824710+InfinibyteF4@users.noreply.github.com>
2023-07-22 22:57:20 -0400

    add a lot of stuff (check description)
    - pressing U on a post or comment will bring you to the poster/commenter's profile (shift+u for new tab)
    
    - pressing E on your posts or comments will start editing them
    
    - pressing minus or equals will shrink or grow expanded images
    
    -pressing Q on a comment that has a show context button will go to the context (shift + q for context in new tab)
    
    - pressing G will bring up a dialog, similar to the "Press a key to go to:" in RES. here's what it looks like:
    
    --- Frontpage Sort ---
    P = posts
    C = comments
    1 = subscribed
    2 = local
    3 = all
    
    --- Everywhere Else ---
    S = saved
    F = frontpage
    U = profile
    I = inbox

--- lemmy-keyboard-navigation.user.js
+++ lemmy-keyboard-navigation.user.js
@@ -11,7 +11,7 @@
 // @icon          https://raw.githubusercontent.com/vmavromatis/Lemmy-keyboard-navigation/main/icon.png?inline=true
 // @homepageURL   https://github.com/vmavromatis/Lemmy-keyboard-navigation
 // @namespace     https://github.com/vmavromatis/Lemmy-keyboard-navigation
-// @description   Easily navigate Lemmy with keyboard arrows
+// @description   Easily navigate Lemmy with your keyboard
 // @run-at        document-end
 // ==/UserScript==
 
@@ -19,13 +19,14 @@
 if (document.querySelectorAll('.lemmy-site').length >= 1){
 
 // Vim key toggle
-// Default: false
-// Set to true for Vim navigation
+// Default: true
+// Set to false for arrow key navigation
 const vimKeyNavigation = true;
 
 // Set selected entry colors
 const backgroundColor = '#373737';
 const textColor = 'white';
+
 // Set navigation keys with keycodes here: https://www.toptal.com/developers/keycode
 var nextKey = 'ArrowDown';
 var prevKey = 'ArrowUp';
@@ -47,6 +48,26 @@ const upvoteKey = 'KeyA';
 const downvoteKey = 'KeyZ';
 const replycommKey = 'KeyR';
 const saveKey = 'KeyS';
+const popupKey = 'KeyG';
+const contextKey = 'KeyQ'
+const smallerimgKey = 'Minus'
+const biggerimgKey = 'Equal'
+const userKey = 'KeyU'
+const editKey = 'KeyE'
+
+const modalCommentsKey = 'KeyC'
+const modalPostsKey = 'KeyP'
+const modalSubscribedKey = 'Digit1';
+const modalLocalKey = 'Digit2';
+const modalAllKey = 'Digit3';
+const modalSavedKey = 'KeyS';
+const modalFrontpageKey = 'KeyF';
+const modalProfileKey = 'KeyU';
+const modalInboxKey = 'KeyI';
+
+const escapeKey = 'Escape';
+var modalMode = 0
+console.log('modalMode: ' + modalMode);
 
 // Stop arrows from moving the page if not using Vim navigation
 window.addEventListener("keydown", function(e) {
@@ -65,6 +86,17 @@ const css = [
 "  color: " + textColor + ";",
 "}"
 ].join("\n");
+
+// dialog box
+var myDialog = document.createElement("dialog");
+document.body.appendChild(myDialog)
+var para = document.createElement("p");
+para.innerText = '--- Frontpage Sort ---\nP = posts\nC = comments\n1 = subscribed\n2 = local\n3 = all\n\n--- Everywhere Else ---\nS = saved\nF = frontpage\nU = profile\nI = inbox\n';
+myDialog.appendChild(para);
+let button = document.createElement("button");
+button.classList.add('CLOSEBUTTON1');
+button.innerHTML = 'Press ESC or G to Close';
+myDialog.appendChild(button);
 
 // Global variables
 let currentEntry;
@@ -94,8 +126,6 @@ if (typeof GM_addStyle !== "undefined") {
     }
 }
 const selectedClass = "selected";
-
-
 
 const targetNode = document.documentElement;
 const config = { childList: true, subtree: true };
@@ -152,89 +182,178 @@ function handleKeyPress(event) {
         return;
     }
 
-    switch (event.code) {
-        case nextKey:
-        case prevKey:
-            previousKey(event);
-            break;
-        case upvoteKey:
-            upVote();
-            break;
-        case downvoteKey:
-            downVote();
-            break;
-        case expandKey:
-            toggleExpand();
-            expand = isExpanded() ? true : false;
-            break;
-        case saveKey:
-            save();
-            break;
-        case openCommentsKey:
-            comments(event);
-            break;
-        case replycommKey:
-            if (window.location.pathname.includes("/post/")) {
+    switch (modalMode) {
+        case modalMode = 0:
+        switch (event.code) {
+            case nextKey:
+            case prevKey:
+                previousKey(event);
+                break;
+            case upvoteKey:
+                upVote();
+                break;
+            case downvoteKey:
+                downVote();
+                break;
+            case expandKey:
+                toggleExpand();
+                expand = isExpanded() ? true : false;
+                break;
+            case smallerimgKey:
+                imgresize(0);
+                break;
+            case biggerimgKey:
+                imgresize(1);
+                break;
+            case saveKey:
+                save();
+                break;
+            case editKey:
+                edit();
+                break;
+            case openCommentsKey:
+                comments(event);
+                break;
+            case popupKey:
+                gotodialog(1);
+                instanceanduser();
+                break;
+            case contextKey:
+                getcontext();
+                break;
+            case replycommKey:
+                if (window.location.pathname.includes("/post/")) {
                 // Allow Mac refresh with CMD+R
                 if (event.key !== 'Meta') {
-                reply(event);
+                    reply(event);
                 }
             } else {
                 community(event);
             }
             break;
-        case openLinkandcollapseKey:
-            if (window.location.pathname.includes("/post/")) {
-                toggleExpand();
-            } else {
-                const linkElement = currentEntry.querySelector(".col.flex-grow-1>p>a")
-                    if (linkElement) {
-                        if (event.shiftKey) {
-                            window.open(linkElement.href);
+            case userKey:
+                visituser();
+                break;
+            case openLinkandcollapseKey:
+                if (window.location.pathname.includes("/post/")) {
+                    toggleExpand();
+                } else {
+                    const linkElement = currentEntry.querySelector(".col.flex-grow-1>p>a")
+                        if (linkElement) {
+                            if (event.shiftKey) {
+                                window.open(linkElement.href);
+                            } else {
+                                linkElement.click();
+                            }
                         } else {
-                            linkElement.click();
+                            comments(event);
                         }
-                    } else {
-                        comments(event);
                     }
+                break;
+            case parentComment:{
+                let targetBlock;
+                if (currentEntry.classList.contains("ms-1")) {
+                    targetBlock = getPrevEntry(currentEntry);
                 }
-            break;
-        case parentComment:{
-            let targetBlock;
-            if (currentEntry.classList.contains("ms-1")) {
-                targetBlock = getPrevEntry(currentEntry);
-            }
-            else if (currentEntry.parentElement.parentElement.parentElement.nodeName == "LI") {
-                targetBlock = currentEntry.parentElement.parentElement.parentElement.getElementsByTagName("article")[0];
-            }
-            if (targetBlock) {
-                if (expand) collapseEntry();
-                selectEntry(targetBlock, true);
-                if (expand) expandEntry();
-            }}
-            break;
-        case nextPageKey:
-        case prevPageKey:{
-            const pageButtons = Array.from(document.querySelectorAll(".paginator>button"));
+                else if (currentEntry.parentElement.parentElement.parentElement.nodeName == "LI") {
+                    targetBlock = currentEntry.parentElement.parentElement.parentElement.getElementsByTagName("article")[0];
+                }
+                if (targetBlock) {
+                    if (expand) collapseEntry();
+                    selectEntry(targetBlock, true);
+                    if (expand) expandEntry();
+                }}
+                break;
+            case nextPageKey:
+            case prevPageKey:{
+                const pageButtons = Array.from(document.querySelectorAll(".paginator>button"));
 
-            if (pageButtons && (document.getElementsByClassName('paginator').length > 0)) {
-                const buttonText = event.code === nextPageKey ? "Next" : "Prev";
-                pageButtons.find(btn => btn.innerHTML === buttonText).click();
-            }
-            // Jump next block of comments
-            if (event.code === nextPageKey) {
+                if (pageButtons && (document.getElementsByClassName('paginator').length > 0)) {
+                    const buttonText = event.code === nextPageKey ? "Next" : "Prev";
+                    pageButtons.find(btn => btn.innerHTML === buttonText).click();
+                }
+                // Jump next block of comments
+                if (event.code === nextPageKey) {
                     commentBlock = getNextEntrySameLevel(currentEntry)
-            }
-            // Jump previous block of comments
-            if (event.code === prevPageKey) {
+                }
+                // Jump previous block of comments
+                if (event.code === prevPageKey) {
                     commentBlock = getPrevEntrySameLevel(currentEntry)
-            }
-
-            if (commentBlock) {
-                if (expand) collapseEntry();
-                selectEntry(commentBlock, true);
-                if (expand) expandEntry();
-            }}
+                }
+                if (commentBlock) {
+                    if (expand) collapseEntry();
+                    selectEntry(commentBlock, true);
+                    if (expand) expandEntry();
+                        }
+        }
+        }break;
+        case modalMode = 1:
+        switch (event.code) {
+            case escapeKey:
+                modalMode = 0;
+                console.log('modalMode: ' + modalMode);
+                break;
+            case popupKey:
+                gotodialog(0);
+                break;
+            case modalSubscribedKey:
+                var subelement = document.querySelectorAll('[title="Shows the communities you\'ve subscribed to"]')[0];
+                subelement.click();
+                gotodialog(0);
+                break;
+            case modalLocalKey:
+                var localelement = document.querySelectorAll('[title="Shows only local communities"]')[0];
+                localelement.click();
+                gotodialog(0);
+                break;
+            case modalAllKey:
+                var allelement = document.querySelectorAll('[title="Shows all communities, including federated ones"]')[0];
+                allelement.click();
+                gotodialog(0);
+                break;
+            case modalSavedKey:
+                if (window.location.pathname.includes("/u/")) {
+                    var savedelement = document.getElementsByClassName("btn btn-outline-secondary pointer")[3];
+                    if (savedelement) {
+                        savedelement.click();
+                        gotodialog(0);
+                    }
+                } else {
+                    instanceanduser(2);
+                }
+                break;
+            case modalFrontpageKey:
+                frontpage();
+                break;
+            case modalProfileKey:
+                var profileelement = document.querySelectorAll('[title="Profile"]')[0];
+                if (profileelement) {
+                    profileelement.click();
+                    gotodialog(0);
+                } else {
+                    instanceanduser(1);
+                }
+                break;
+            case modalInboxKey:
+                var notifelement = document.getElementsByClassName("nav-link d-inline-flex align-items-center d-md-inline-block")[2];
+                if (notifelement) {
+                    notifelement.click();
+                    gotodialog(0);
+                } else {
+                    console.log('Not logged in!');
+                }
+                break;
+            case modalCommentsKey:
+                var commentsbutton = document.getElementsByClassName("pointer btn btn-outline-secondary")[1];
+                commentsbutton.click();
+                gotodialog(0);
+                break;
+            case modalPostsKey:
+                var postsbutton = document.getElementsByClassName("pointer btn btn-outline-secondary")[0];
+                postsbutton.click();
+                gotodialog(0);
+                break;
+        }
     }
 }
 
@@ -367,6 +486,65 @@ function downVote() {
     }
 }
 
+function gotodialog(n) {
+
+    const closeButton = document.getElementsByClassName("CLOSEBUTTON1")[0];
+    closeButton.addEventListener("click", () => {
+        myDialog.close();
+        modalMode = 0;
+        console.log('modalMode: ' + modalMode);
+    });
+    if (n == 1) {
+        myDialog.showModal();
+        modalMode = 1;
+        console.log('modalMode: ' + modalMode);
+    }
+
+    if (n == 0) {
+        myDialog.close();
+        modalMode = 0;
+        console.log('modalMode: ' + modalMode);
+    }
+}
+
+function instanceanduser(n) {
+    var currentinstance = window.location.origin
+    var dropdownuser = document.getElementsByClassName("btn dropdown-toggle")[0];
+    var username = dropdownuser.textContent
+
+    if (n == 0) {
+        window.location.replace(currentinstance)
+    }
+    if (n == 1) {
+        if (username) {
+            var userlink = currentinstance+"/u/"+username
+            window.location.replace(userlink)
+        } else {
+            console.log('Not logged in!')
+            frontpage();
+        }
+    }
+    if (n == 2) {
+        if (username) {
+            var savedlink = currentinstance+"/u/"+username+"?page=1&sort=New&view=Saved"
+            window.location.replace(savedlink)
+        } else {
+            console.log('Not logged in!')
+            frontpage();
+        }
+    }
+}
+
+function frontpage() {
+    var homeelement = document.getElementsByClassName("d-flex align-items-center navbar-brand me-md-3 active")[0];
+    if (homeelement) {
+        homeelement.click();
+        gotodialog(0);
+    } else {
+        instanceanduser(0);
+    }
+}
+
 function reply(event) {
     const replyButton = currentEntry.querySelector("button[data-tippy-content='reply']");
 
@@ -386,6 +564,17 @@ function community(event) {
         }
 }
 
+function visituser() {
+    var userbutton = currentEntry.getElementsByClassName("person-listing d-inline-flex align-items-baseline text-info")[0]
+    var userHREF = currentEntry.getElementsByClassName("person-listing d-inline-flex align-items-baseline text-info")[0].href
+
+    if (event.shiftKey) {
+        window.open(userHREF)
+    } else {
+        userbutton.click();
+    }
+}
+
 function comments(event) {
     if (event.shiftKey) {
         window.open(
@@ -393,6 +582,54 @@ function comments(event) {
         );
     } else {
         currentEntry.querySelector("a.btn[title*='Comment']").click();
+    }
+}
+
+
+function getcontext() {
+    var contextbutton = currentEntry.querySelector('[title="Show context"]')
+    var contextHREF = currentEntry.querySelector('[title="Show context"]').href
+
+    if (event.shiftKey) {
+        window.open(contextHREF)
+    } else {
+        contextbutton.click();
+    }
+}
+
+var maxsize = 0
+console.log('maxsize '+maxsize)
+
+function imgresize(n) {
+    var expandedimg = currentEntry.getElementsByClassName("overflow-hidden pictrs-image img-fluid img-expanded slight-radius")[0];
+    var expandedheight = expandedimg.height
+    var expandedwidth = expandedimg.width
+    var expandedheightbefore = expandedheight
+    var expandedwidthbefore = expandedwidth
+
+    if (n == 0) {
+        expandedheight = expandedheight/1.15
+        expandedwidth = expandedwidth/1.15
+        expandedimg.style.height = expandedheight+'px'
+        expandedimg.style.width = expandedwidth+'px'
+        maxsize = 0
+        console.log('maxsize '+maxsize)
+    }
+
+    if (n == 1) {
+        expandedheight = expandedheight*1.15
+        expandedwidth = expandedwidth*1.15
+        expandedimg.style.width = expandedwidth+'px'
+        expandedimg.style.height = expandedheight+'px'
+
+        if (maxsize == 1) {
+            expandedimg.style.width = expandedwidthbefore+'px'
+            expandedimg.style.height = expandedheightbefore+'px'
+        }
+        if (expandedimg.width !== Math.round(expandedwidth) || expandedimg.height !== Math.round(expandedheight)) {
+            maxsize = 1
+            console.log('maxsize '+maxsize)
+        }
     }
 }
 
@@ -411,6 +648,17 @@ function save() {
         } else if (unsaveButton) {
             unsaveButton.click();
         }
+    }
+}
+
+function edit() {
+    var editButton = currentEntry.querySelector("button[aria-label='Edit']")
+    var moreButton = currentEntry.querySelector("button[aria-label='more']");
+
+    if (editButton) {
+        editButton.click();
+    } else {
+        moreButton.click();
     }
 }
 
@@ -434,6 +682,7 @@ function toggleExpand() {
                     currentEntry.offsetHeight - imgContainer.offsetHeight + 10
                 );
             }, true);
+            currentEntry.getElementsByClassName("offset-sm-3 my-2 d-none d-sm-block")[0].className = "my-2 d-none d-sm-block";
         }
     }
 
