0ab27f4b975a792545b58e7e15965774e1a7d80a
Vasilis Mavromatis <8668731+vmavromatis@users.noreply.github.com>
2023-08-30 01:03:54 +0300

    Update newlemmy.js

--- newlemmy.js
+++ newlemmy.js
@@ -1,24 +1,4 @@
-// ==UserScript==
-// @name          lemmy-keyboard-navigation
-// @match         https://*/*
-// @grant         none
-// @version       2.2
-// @author        vmavromatis
-// @author        howdy@thesimplecorner.org
-// @author        InfinibyteF4
-// @author        aglidden
-// @license       GPL3
-// @icon          https://raw.githubusercontent.com/vmavromatis/Lemmy-keyboard-navigation/main/icon.png?inline=true
-// @homepageURL   https://github.com/vmavromatis/Lemmy-keyboard-navigation
-// @namespace     https://github.com/vmavromatis/Lemmy-keyboard-navigation
-// @description   Easily navigate Lemmy with your keyboard
-// @run-at        document-end
-// ==/UserScript==
-
 /*global window,console,localStorage,sessionStorage,document,GM_addStyle,PRO_addStyle,addStyle,MutationObserver,location*/
-
-//isLemmySite
-if (document.querySelectorAll('.lemmy-site').length >= 1) {
 
 // DEBUGGING (ignore me!)
 //localStorage.clear();
@@ -1498,6 +1478,4 @@ function scrollPage(y) {
       top: y
     });
   }
-}
-
 }
