5c62ae68c572071dbc485128a39bb272de5ebba5
Vasilis Mavromatis <8668731+vmavromatis@users.noreply.github.com>
2023-08-31 23:34:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -11,27 +11,8 @@
 // @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/separate-js-files/oldlemmy.js
 // @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/separate-js-files/newlemmy.js
 // @icon          https://raw.githubusercontent.com/vmavromatis/Lemmy-keyboard-navigation/main/icon.png?inline=true
-// @homepageURL   https://github.com/vmavromatis/Lemmy-keyboard-navigation/tree/add-mlmym-support
-// @namespace     https://github.com/vmavromatis/Lemmy-keyboard-navigation/tree/add-mlmym-support
+// @homepageURL   https://github.com/vmavromatis/Lemmy-keyboard-navigation
+// @namespace     https://github.com/vmavromatis/Lemmy-keyboard-navigation
 // @description   Easily navigate Lemmy with your keyboard
 // @run-at        document-end
 // ==/UserScript==
-
-//if New Lemmy
-if (document.querySelectorAll('.lemmy-site').length >= 1) {
-        var src = "https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/separate-js-files/newlemmy.js";
-        var newScript = document.createElement("script");
-        newScript.type = "text/javascript";
-        newScript.setAttribute("async", "true");
-        newScript.setAttribute("src", src);
-        document.body.appendChild(newScript);
-}
-//if Old Lemmy (mlmym)
-else if (document.querySelectorAll('.spacer>a>.icon').length >= 1){
-        src = "https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/separate-js-files/oldlemmy.js";
-        newScript = document.createElement("script");
-        newScript.type = "text/javascript";
-        newScript.setAttribute("async", "true");
-        newScript.setAttribute("src", src);
-        document.body.appendChild(newScript);
-}
