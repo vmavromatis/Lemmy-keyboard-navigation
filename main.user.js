91f7e05359ee97f5b5ef5156512625a0b6f5c0ab
Vasilis Mavromatis <8668731+vmavromatis@users.noreply.github.com>
2024-01-27 10:21:53 +0200

    2.7

--- main.user.js
+++ main.user.js
@@ -2,13 +2,13 @@
 // @name          lemmy-keyboard-navigation
 // @match         https://*/*
 // @grant         none
-// @version       2.6
+// @version       2.7
 // @author        vmavromatis
 // @author        InfinibyteF4
 // @author        aglidden
 // @author        howdy-tsc
 // @license       GPL3
-// @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation.user.js#md5=a3fcc5f604876f83afc9ea6f6f52bd82
+// @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation.user.js#md5=00869c5d5a124367afc8bafb9db39e0d
 // @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation-mlmym.user.js#md5=c7542b4ba8de41cf87e1386791a6da7d
 // @icon          https://raw.githubusercontent.com/vmavromatis/Lemmy-keyboard-navigation/main/icon.png?inline=true
 // @homepageURL   https://github.com/vmavromatis/Lemmy-keyboard-navigation
