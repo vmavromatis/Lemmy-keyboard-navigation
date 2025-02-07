7c49047192ea96b157472074c4cc284a24243be8
InfinibyteF4 <75824710+InfinibyteF4@users.noreply.github.com>
2025-02-06 19:23:08 -0500

    update md5 hash

--- main.user.js
+++ main.user.js
@@ -8,7 +8,7 @@
 // @author        aglidden
 // @author        howdy-tsc
 // @license       GPL3
-// @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation.user.js#md5=04c5bef05b3b9d6e0b0032c73133c781
+// @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation.user.js#md5=8c7c5e51f31290b8a48f4169d553d87b
 // @require       https://github.com/vmavromatis/Lemmy-keyboard-navigation/raw/main/lemmy-keyboard-navigation-mlmym.user.js#md5=53de470a0975b0b608377fd55c649530
 // @icon          https://raw.githubusercontent.com/vmavromatis/Lemmy-keyboard-navigation/main/icon.png?inline=true
 // @homepageURL   https://github.com/vmavromatis/Lemmy-keyboard-navigation
